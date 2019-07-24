e4641b65fe018bed4597c770118913eea9a27d5a
zhangsan <zhangsan@qq.com>
2019-07-24 20:25:27 +0800

    添加日志配置

--- src/test/java/com/boot/demo/DemoApplicationTests.java
+++ src/test/java/com/boot/demo/DemoApplicationTests.java
@@ -3,6 +3,8 @@ package com.boot.demo;
 import com.boot.demo.entity.Emp;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,11 +13,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     Emp emp;
     @Test
     public void contextLoads() {
         System.out.println(emp);
+    }
+
+    @Test
+    public void logTest() {
+        System.out.println(emp);
+        //默认日志等级为info
+        logger.trace("trace");
+        logger.debug("debug");
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
     }
 
 }
